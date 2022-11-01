de4f40f76c7fa5969e457298bd48691a6fb3f309
gustafakemal <gustafakemal11@gmail.com>
2022-11-01 21:00:52 +0700

    database

---
+++ blacklist.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2022 at 02:59 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blacklist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ktp`
+--
+
+CREATE TABLE `ktp` (
+  `NIK` varchar(100) NOT NULL,
+  `NamaLengkap` varchar(100) DEFAULT NULL,
+  `Tempat` varchar(100) DEFAULT NULL,
+  `TanggalLahir` datetime DEFAULT NULL,
+  `Blacklist` varchar(1) DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ktp`
+--
+ALTER TABLE `ktp`
+  ADD PRIMARY KEY (`NIK`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
